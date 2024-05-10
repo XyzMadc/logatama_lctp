583de7d13a57abc39072e8acbd17232b079c2c02
Codewarriorrs <isyamuhammad341@gmail.com>
2024-05-10 21:09:07 +0700

    add migration for user and soal's table

--- database/migrations/2024_05_10_140553_create_soal_table.php
+++ database/migrations/2024_05_10_140553_create_soal_table.php
@@ -4,18 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('soal', function (Blueprint $table) {
             $table->id();
-            $table->text('username', 20);
-            $table->string('password', 255)->nullable();
-            $table->text('nama')->default('-');
-            $table->string('email', 100)->nullable();
+            $table->string("soal");
+            $table->string("jawaban");
+            $table->string("jawaban_benar");
         });
     }
 
@@ -24,6 +24,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('soal');
     }
 };
